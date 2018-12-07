c36de2b8d47313c0fbb20b6e1ccc797299ec1f04
axel <axel.jean-caurant@univ-lr.fr>
2018-12-07 15:27:54 +0100

    modified config for development

--- script/create_services.sh
+++ script/create_services.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 docker run -d -t -p 8983:8983 -v $DIR/../solr/config:/imported_config --name newseye_solr solr:7.5.0 solr-create -c hydra-development -d /imported_config
 docker run -d -t -p 8984:8080 --name newseye_fcrepo lyrasis/fcrepo:4.7.4
-docker run -d -t -p 5433:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=newseye_development --name newseye_postgres postgres:11
+docker run -d -t -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=newseye_development --name newseye_postgres postgres:11
