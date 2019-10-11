6f4b55fc53e5a69d9c245fee1e0c07159632acd1
axel <axel.jean-caurant@univ-lr.fr>
2019-10-11 09:52:14 +0200

    fix investigate task display

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  after_create :generate_subtasks
+  # after_create :generate_subtasks
 
   def subtasks
     case self.task_type
@@ -13,7 +13,12 @@ class Task < ApplicationRecord
           if subtask
             out_sub_tasks << subtask
           else
-            Rails.logger.debug("Subtask not found :(")
+            Rails.logger.debug("Subtask not found, creating it...")
+            data = PersonalResearchAssistantService.get_analysis_task sub_uuid
+            Task.create(user: self.user, status: data['task_status'], uuid: data['uuid'],
+                        started: data['task_started'], finished: data['task_finished'],
+                        task_type: data['task_type'], parameters: data['task_parameters'],
+                        results: data['task_result'], subtask: true)
           end
         end
       end
@@ -23,21 +28,21 @@ class Task < ApplicationRecord
     end
   end
 
-  private
-
-  def generate_subtasks
-    unless self.results.nil?
-      if self.task_type == 'investigator'
-        subs_uuids = self.results['result']
-        subs_uuids.keys.each do |sub_uuid|
-          data = PersonalResearchAssistantService.get_analysis_task sub_uuid
-          Task.create(user: self.user, status: data['task_status'], uuid: data['uuid'],
-                      started: data['task_started'], finished: data['task_finished'],
-                      task_type: data['task_type'], parameters: data['task_parameters'],
-                      results: data['task_result'], subtask: true)
-        end
-      end
-    end
-  end
+  # private
+  #
+  # def generate_subtasks
+  #   unless self.results.nil?
+  #     if self.task_type == 'investigator'
+  #       subs_uuids = self.results['result']
+  #       subs_uuids.keys.each do |sub_uuid|
+  #         data = PersonalResearchAssistantService.get_analysis_task sub_uuid
+  #         Task.create(user: self.user, status: data['task_status'], uuid: data['uuid'],
+  #                     started: data['task_started'], finished: data['task_finished'],
+  #                     task_type: data['task_type'], parameters: data['task_parameters'],
+  #                     results: data['task_result'], subtask: true)
+  #       end
+  #     end
+  #   end
+  # end
 
 end
